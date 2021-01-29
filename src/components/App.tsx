26e4fff2c7975ff4c08cbbda09551cc4b5ce54c3
Blaise Gratton <blaisegratton@gmail.com>
2021-01-28 22:39:01 -0600

    adjust position of add button; add title bar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
+import AddIcon from '@material-ui/icons/Add'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
+import '../App.css'
 import { AppState } from '../redux/types'
 import { FormState } from '../redux/reducers'
 import OptionsRow from './OptionsRow'
@@ -16,10 +21,25 @@ interface Props {
 
 const App = ({ rows, addRow, images }: Props) => (
   <section>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h4">
+          Dog Fetcher
+        </Typography>
+      </Toolbar>
+    </AppBar>
     <section>
       {rows.map((row, index) => <OptionsRow rowState={row} index={index} key={row.key}/>)}
     </section>
-    <Button color="primary" variant="contained" onClick={() => addRow()}>Add Row</Button>
+    <section className="add-row-section">
+      <Button color="primary"
+              variant="contained"
+              onClick={() => addRow()}
+              startIcon={<AddIcon />}
+      >
+        Add Row
+      </Button>
+    </section>
     <ImageModal />
   </section>
 )
